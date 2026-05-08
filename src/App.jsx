867f18e610d095d72fa79563577809c84d6726aa
Jinsu Kim <Jake.sw.engineer@gmail.com>
2026-05-08 22:28:33 +0900

    Init React GitHub Pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <p>My GitHub Pages React Blog</p>
+    </main>
+  );
+}
+
+export default App;
